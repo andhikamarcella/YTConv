ef7ee3521586e24222b456689970326016193cf7
cellauu <andhikamarcellafernanda@gmail.com>
2026-09-19 21:03:19 +0700

    release: update iSH installer to 1.7.7

--- cli/scripts/install-ish.sh
+++ cli/scripts/install-ish.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-VERSION="1.7.6"
-RAW_BASE="https://raw.githubusercontent.com/andhikamarcella/YTConv/release/ytconv-1.7.6/cli"
+VERSION="1.7.7"
+RAW_BASE="https://raw.githubusercontent.com/andhikamarcella/YTConv/release/ytconv-1.7.7/cli"
 APP_DIR="/usr/local/lib/ytconv-ish"
 APP_FILE="$APP_DIR/ytconv.py"
 CORE_FILE="$APP_DIR/ytconv-core.py"
